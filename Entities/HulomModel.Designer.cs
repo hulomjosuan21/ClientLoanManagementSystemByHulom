c8293c1e88f9289a0b7cc53f993bc21ba9997e17
Josuan <hulomjosuan@gmail.com>
2024-05-31 06:44:56 +0800

    asdsd

--- Entities/HulomModel.Designer.cs
+++ Entities/HulomModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\FOR CLONE\Source\Repos\hulomjosuan21\ClientLoanManagementSystemByHulom\Entities\HulomModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Josuan\source\repos\hulomjosuan21\ClientLoanManagementSystemByHulom\Entities\HulomModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
